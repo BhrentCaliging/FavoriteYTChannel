96c415be9a3a8ca378488d7dc54b3c76c79d0559
Bhrent Caliging <55260865+BhrentCaliging@users.noreply.github.com>
2020-12-01 12:59:05 +0800

    Created My Favorite Youtube Channels app activity

---
+++ app/src/main/java/com/example/favoriteytchannel/handlers/ChannelHandler.kt
@@ -0,0 +1,33 @@
+package com.example.favoriteytchannel.handlers
+
+import com.example.favoriteytchannel.models.Channel
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class ChannelHandler {
+     var database: FirebaseDatabase
+     var channelRef: DatabaseReference
+
+    init {
+        database = FirebaseDatabase.getInstance()
+        channelRef = database.getReference("yt_channels")
+    }
+
+    fun create(channel: Channel): Boolean{
+        val id = channelRef.push().key
+        channel.id = id
+
+        channelRef.child(id!!).setValue(channel)
+        return true
+    }
+
+    fun update(channel: Channel): Boolean{
+        channelRef.child(channel.id!!).setValue(channel)
+        return true
+    }
+
+    fun delete(channel: Channel): Boolean{
+        channelRef.child(channel.id!!).removeValue()
+        return true
+    }
+}
